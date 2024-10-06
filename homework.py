fda27330dafbce48e6d5d5e9fe8cebf8ec13cfa3
dilara2016 <m.123.hunain@gmail.com>
2024-10-06 18:52:48 +0200

    homework

---
+++ homework.py
@@ -0,0 +1,13 @@
+#storing values
+tree1 = 35
+tree2 = 98
+tree3 = 83
+tree4 = 53
+tree5 = 58
+
+#finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5trees is:",sum)
+#finding the average of trees
+average = sum/7
+print("the average of all the tree is:",average)
